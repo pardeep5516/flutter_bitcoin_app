5d5f0f596e810147704f913b42002b5a9383e444
pardeep5516 <43162246+pardeep5516@users.noreply.github.com>
2020-02-15 20:52:28 +0530

    initial comment

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bitcoin_app/price_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
